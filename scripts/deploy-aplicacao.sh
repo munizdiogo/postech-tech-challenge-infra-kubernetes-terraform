bca9724ef48c087810ed7a27309c97454aaf681e
Diogo Muniz <diogomuniz.web@gmail.com>
2023-11-08 07:26:55 -0300

    Refatorado código para melhorar manutenção e legibilidade

---
+++ scripts/deploy-aplicacao.sh
@@ -0,0 +1,5 @@
+kubectl delete -f k8s/deployment.yaml
+kubectl apply -f k8s/deployment.yaml
+kubectl apply -f k8s/public-lb.yaml
+kubectl apply -f k8s/private-lb.yaml
+kubectl apply -f k8s/cluster-autoscaler.yaml
